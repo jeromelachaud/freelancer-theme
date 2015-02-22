259a0e9a3a59eb2c7e95b7af8af08c111db3c835
Ricardo Nunes de Miranda <nm.ricardo@gmail.com>
2015-02-21 23:34:21 -0200

    Fixing the text of the validation of email

--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -183,7 +183,7 @@
             //                                                     EMAIL
             // ---------------------------------------------------------
             if ($this.attr("type") !== undefined && $this.attr("type").toLowerCase() === "email") {
-              message = "Not a valid email address<!-- data-validator-validemail-message to override -->";
+              message = "Not a valid email address<!-- data-validation-validemail-message to override -->";
               if ($this.data("validationValidemailMessage")) {
                 message = $this.data("validationValidemailMessage");
               } else if ($this.data("validationEmailMessage")) {
@@ -791,7 +791,7 @@
 				name: "Validemail",
 				type: "regex",
 				regex: "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\.[A-Za-z]{2,4}",
-				message: "Not a valid email address<!-- data-validator-validemail-message to override -->"
+				message: "Not a valid email address<!-- data-validation-validemail-message to override -->"
 			},
 			passwordagain: {
 				name: "Passwordagain",
