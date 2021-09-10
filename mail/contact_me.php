a4b7a52ef2d9af8a56a4d8f07fc2476cacb7dc42
dh0725 <gidion3820@gmail.com>
2021-09-10 17:48:23 +0900

    UPDATE about COMMIT

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -22,7 +22,7 @@ $message = $_POST['message'];
 
 if (empty($_POST['_gotcha'])) { // If hidden field was filled out (by spambots) don't send!
     // Create the email and send the message
-    $to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $to = 'gidion@kakao.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
     $email_subject = "Website Contact Form:  $name";
     $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
     $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
